e1dcb707d058619ab4841d560d87272ce1aa7947
chenguangxian <89511103+1-ASCII-1@users.noreply.github.com>
2024-02-02 10:11:24 +0800

    init

---
+++ src/test/java/com/ifeng/logbacklearn/LogbackLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifeng.logbacklearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
